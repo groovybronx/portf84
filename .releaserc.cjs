e1926b6c834fa9305dd20e152e810f72c536d2d9
groovybronx <michelswork@yahoo.fr>
2026-01-09 20:51:14 +0100

    fix(ci): remove git plugin from semantic-release to avoid protected branch issues

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -17,13 +17,5 @@ module.exports = {
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'src-tauri/Cargo.toml'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-        gitCommitOptions: ['--no-verify'],
-      },
-    ],
   ],
 };
